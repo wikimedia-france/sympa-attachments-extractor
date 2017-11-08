ef6a9181404caf76ae65fbb5630402eafea03ea7
Ash_Crow <sylvain@ashtree.eu>
2017-11-08 17:10:10 +0100

    Main script

--- extractor.py
+++ extractor.py
@@ -1,6 +1,76 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import requests
+import os
+import configparser
 from bs4 import BeautifulSoup
+import csv
 
+
+# Read the config file
+config = configparser.ConfigParser()
+config.read(os.path.dirname(__file__) + '/config.ini')
+
+BASE_DIR = config.get('sympa', 'base_dir')
+SERVER_NAME = config.get('sympa', 'server_name')
+BASE_URL = config.get('sympa', 'base_url')
+OUTPUT_FILE = config.get('sympa', 'output_file')
+MAILING_LISTS = config.get('sympa', 'mailing_lists').split(',')
+all_files = []
+
+for ml in MAILING_LISTS:
+    print("Parsing emails from {}@{} mailing list".format(ml, SERVER_NAME))
+    ml_dir = BASE_DIR + '/' + ml + '@' + SERVER_NAME
+
+    months = os.listdir(ml_dir)
+
+    for m in months:
+        monthly_dir = ml_dir + '/' + m
+        for entry in os.scandir(monthly_dir):
+
+            """
+            If there a mail has an attachment, there will will be both
+            a msgXXXXX dir along the msgXXXXX.html file, so for each
+            msgXXXXX dir, we parse the matching html file.
+            """
+            if entry.is_dir() and entry.name[0:3] == 'msg':
+                mail_file = entry.name + '.html'
+                email_file = monthly_dir + '/' + mail_file
+
+                with open(email_file, 'r') as f:
+                    webpage = f.read()
+                    soup = BeautifulSoup(webpage, 'html.parser')
+
+                    current_mail = {}
+                    current_mail['URL'] = BASE_URL + m + '/' + mail_file
+
+                    for i in soup.find_all('li'):
+                        if hasattr(i, 'strong') and hasattr(i.strong, 'text'):
+                            fieldname = i.strong.text
+
+                            if fieldname in ['Date', 'To', 'From', 'Subject']:
+                                start = len(fieldname) + 2
+                                current_mail[fieldname] = i.text[start:]
+
+                    for i in soup.find_all('p'):
+                        if 'Attachment:' in i.text:
+                            current_file = current_mail.copy()
+                            current_file['Attachment'] = i.a.text
+
+                    all_files.append(current_file)
+
+
+with open(OUTPUT_FILE, 'w') as csvfile:
+    fieldnames = [
+        'Date',
+        'Subject',
+        'From',
+        'To',
+        'Attachment',
+        'URL']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    writer.writeheader()
+
+    for f in all_files:
+        writer.writerow(f)
